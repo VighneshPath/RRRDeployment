1c8130bbf7ae124eae09bafe358fad258b931e01
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-28 16:37:04 +0530

    Add terraform

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {
+    bucket = "gurukul-vighnesh"
+    key    = "workflow/terraform.tfstate"
+    region = "us-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+  
+
+output "instance_ip_addr" {
+  value       = aws_instance.app_server.private_ip
+  description = "The private IP address of the main server instance."
+}	
