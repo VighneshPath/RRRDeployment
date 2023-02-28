99d2f3fc85f522031fa3f83651f6f96d3fa785ec
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-28 18:25:09 +0530

    Fix github output syntax

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name = "vighnesh-terraform"
 
   tags = {
     Name = "gurukul-vighnesh-esop"
