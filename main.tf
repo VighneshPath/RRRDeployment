12ae48d465d3771894c0daf31575c80fe29af978
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-28 18:26:03 +0530

    Format tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  key_name = "vighnesh-terraform"
+  key_name      = "vighnesh-terraform"
 
   tags = {
     Name = "gurukul-vighnesh-esop"
