104174c423bb33e14e613ecc0aa55bb9e8f19c6d
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-01 10:38:57 +0530

    Add vpc rules, now using public ip

--- main.tf
+++ main.tf
@@ -22,14 +22,57 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   key_name      = "vighnesh-terraform"
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "gurukul-vighnesh-esop"
   }
 }
 
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+  tags ={
+    "name"="Traffic Rules"
+  }
+}
 
 output "instance_ip_addr" {
-  value       = aws_instance.app_server.private_ip
-  description = "The private IP address of the main server instance."
+  value       = aws_instance.app_server.public_ip
+  description = "The public IP address of the main server instance."
 } 
