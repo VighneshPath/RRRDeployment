3b8e9102da4c31b860b7d11cf7a6e20fa1dd4071
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-01 11:38:11 +0530

    Update key pair

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
-  key_name               = "vighnesh-terraform"
+  key_name               = "gurukul-vighnesh"
   vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
