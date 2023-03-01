6963a7e16ea4e03a139ecf559864e7ff07eccd49
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-01 15:15:15 +0530

    sudo

--- start.sh
+++ start.sh
@@ -6,6 +6,4 @@ sudo yum install -y java-17-amazon-corretto-devel
 
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill -9
 
-export MICRONAUT_SERVER_PORT=80
-
-nohup java -jar esop-trading/build/libs/esop-trading-0.1-all.jar > /dev/null 2>&1&
+nohup sudo MICRONAUT_SERVER_PORT=80 java -jar esop-trading/build/libs/esop-trading-0.1-all.jar > /dev/null 2>&1&
