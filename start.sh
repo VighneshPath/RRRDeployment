7f4f6d081e905dde6e387fdd93f0c0d4bcfc527c
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-01 15:04:19 +0530

    Test

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sudo yum update
 
 sudo yum install -y java-17-amazon-corretto-devel
 
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill -9
 
 export MICRONAUT_SERVER_PORT=80
 
