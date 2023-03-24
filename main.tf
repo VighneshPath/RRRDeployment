f2a1a4e6e18c2cb01e59eb8c5e99b888f82a46ba
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-24 16:12:07 +0530

    Check w/o route table

--- main.tf
+++ main.tf
@@ -29,20 +29,9 @@ resource "aws_subnet" "subnet" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "vpc-019c09a1a0c5b4f6b"
   tags = {
     Name = "Custom Gateway"
-  }
-}
-
-resource "aws_default_route_table" "route_table" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "Default route table"
   }
 }
 
